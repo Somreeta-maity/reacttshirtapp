5af6ee0bc94bf4d36770df873fd99bf88705585b
Somreeta Maity <ateermos@gmail.com>
2020-07-21 17:55:09 +0530

    Add files via upload

--- order.js
+++ order.js
@@ -1,38 +1,60 @@
-const mongoose = require("mongoose");
-const { ObjectId } = mongoose.Schema;
+const { Order, ProductCart } = require("../models/order");
 
-const ProductCartSchema = new mongoose.Schema({
-  product: {
-    type: ObjectId,
-    ref: "Product"
-  },
-  name: String,
-  count: Number,
-  price: Number
-});
+exports.getOrderById = (req, res, next, id) => {
+  Order.findById(id)
+    .populate("products.product", "name price")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "NO order found in DB"
+        });
+      }
+      req.order = order;
+      next();
+    });
+};
 
-const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
-
-const OrderSchema = new mongoose.Schema(
-  {
-    products: [ProductCartSchema],
-    transaction_id: {},
-    amount: { type: Number },
-    address: String,
-    updated: Date,
-    status:{
-      type:String,
-      default : "Received",
-      enum: ["Cancelled","Delivered","Shipped", "Processing","Received"]
-    },
-    user: {
-      type: ObjectId,
-      ref: "User"
+exports.createOrder = (req, res) => {
+  req.body.order.user = req.profile;
+  const order = new Order(req.body.order);
+  order.save((err, order) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Failed to save your order in DB"
+      });
     }
-  },
-  { timestamps: true }
-);
+    res.json(order);
+  });
+};
 
-const Order = mongoose.model("Order", OrderSchema);
+exports.getAllOrders = (req, res) => {
+  Order.find()
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No orders found in DB"
+        });
+      }
+      res.json(order);
+    });
+};
 
-module.exports = { Order, ProductCart };
+exports.getOrderStatus = (req, res) => {
+  res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateStatus = (req, res) => {
+  Order.update(
+    { _id: req.body.orderId },
+    { $set: { status: req.body.status } },
+    (err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Cannot update order status"
+        });
+      }
+      res.json(order);
+    }
+  );
+};
